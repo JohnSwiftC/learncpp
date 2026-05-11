6816b8954593f56f4f64ac018c460afce7387844
John Swift <johnswift@live.com>
2026-05-11 00:52:39 -0400

    Here we are lmao

---
+++ intro/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  // cpp already presents as the most annoying language
+  // of all time
+
+  int a{1};
+
+  std::cout << "Why does cpp have this stream operator??" << '\n';
+
+  std::cout << a;
+
+  return 0;
+}

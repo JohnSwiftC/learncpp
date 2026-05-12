b75d8adadbb4e2d769c2f5b0bb0a984af6721146
JohnSwiftC <johnswift@live.com>
2026-05-11 21:21:54 -0400

    Crank out an easy quiz

---
+++ chapter7/three.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int accumulate(int x);
+
+int main()
+{
+    std::cout << accumulate(4) << '\n'; // prints 4
+    std::cout << accumulate(3) << '\n'; // prints 7
+    std::cout << accumulate(2) << '\n'; // prints 9
+    std::cout << accumulate(1) << '\n'; // prints 10
+
+    return 0;
+}
+
+// Another simple solution here
+
+int accumulate(int x)
+{
+    static int sum{0};
+
+    sum += x;
+
+    return sum;
+}

65bee50b9964af225c469598fb9da81c109064c1
John Swift <johnswift@live.com>
2026-05-17 03:06:27 -0400

    Improved vector!

--- dynamic/badvec.h
+++ dynamic/badvec.h
@@ -2,15 +2,25 @@
 #define BADVEC
 
 #include <cstddef>
+#include <optional>
 template <typename T> struct badvec {
 
   using Index = std::ptrdiff_t;
 
-  T *data;
-  Index ptr;
   std::size_t cap;
+  Index ptr;
+  T *data;
 
   explicit badvec(std::size_t size) : cap{size}, ptr{0}, data{new T[size]} {}
+
+  badvec(const badvec<T> &other)
+      : cap{other.cap}, ptr{other.ptr}, data{new T[other.cap]} {
+    for (std::size_t i{0}; i < other.cap; ++i) {
+      data[i] = other.data[i];
+    }
+  }
+
+  ~badvec() { delete[] data; }
 
   T &operator[](Index index) { return data[index]; }
 
@@ -18,6 +28,19 @@ template <typename T> struct badvec {
 
   void push(T value) {
     if (static_cast<std::size_t>(ptr) == cap) {
+      T *new_data = new T[cap + 1];
+
+      for (std::size_t i{0}; i < cap; ++i) {
+        new_data[i] = data[i];
+      }
+
+      delete[] data;
+      data = new_data;
+      cap++;
+
+      data[ptr] = value;
+      ptr++;
+
       return;
     }
 
@@ -27,9 +50,9 @@ template <typename T> struct badvec {
     return;
   }
 
-  T pop() {
+  std::optional<T> pop() {
     if (ptr == 0) {
-      return data[0];
+      return std::nullopt;
     }
 
     --ptr;

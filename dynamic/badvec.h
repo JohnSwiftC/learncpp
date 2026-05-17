11c2e69d0013c50a0e3ffb333056b82045d3431b
John Swift <johnswift@live.com>
2026-05-17 03:28:10 -0400

    Rule of 5 on my vec

--- dynamic/badvec.h
+++ dynamic/badvec.h
@@ -13,14 +13,43 @@ template <typename T> struct badvec {
 
   explicit badvec(std::size_t size) : cap{size}, ptr{0}, data{new T[size]} {}
 
-  badvec(const badvec<T> &other)
+  badvec(const badvec &other)
       : cap{other.cap}, ptr{other.ptr}, data{new T[other.cap]} {
-    for (std::size_t i{0}; i < other.cap; ++i) {
+    for (std::size_t i{0}; i < other.ptr; ++i) {
       data[i] = other.data[i];
     }
   }
 
   ~badvec() { delete[] data; }
+
+  // All of these functions should be noexcept
+  // many container types will only move
+  // items if they are marked as noexcept,
+  // otherwise they fall back to copying
+
+  // Move constructor
+  // the && ref signifies this only is defined
+  // on an rvalue (move semantics), also see
+  // std::move
+  badvec(badvec &&other) noexcept
+      : cap{other.cap}, ptr{other.ptr}, data{other.data} {
+    other.data = nullptr;
+    other.ptr = 0;
+    other.cap = 0;
+  }
+
+  friend void swap(badvec &a, badvec &b) noexcept {
+    using std::swap;
+    swap(a.data, b.data);
+    swap(a.cap, b.cap);
+    swap(a.ptr, b.ptr);
+  }
+
+  // Copy assignment
+  badvec &operator=(badvec other) noexcept {
+    swap(*this, other);
+    return *this;
+  }
 
   T &operator[](Index index) { return data[index]; }
 

6a5add1e409e8d917d56f000f2abf39bcc9b62b1
John Swift <johnswift@live.com>
2026-05-24 19:45:04 -0400

    A static array class with inheritance, indexing, unique_ptr

--- inherit/main.cpp
+++ inherit/main.cpp
@@ -2,6 +2,7 @@
 #include <initializer_list>
 #include <iostream>
 #include <memory>
+#include <utility>
 
 // A container parent class
 // with a size and a size getter
@@ -14,16 +15,38 @@ public:
   Container(std::size_t size) : m_size{size} {}
 };
 
-// A vector class which publically inherits container
-template <typename T> class Vector : public Container {
+// An array class which publically inherits container
+template <typename T> class Array : public Container {
 public:
   std::unique_ptr<T[]> m_data;
-  Vector(std::size_t size)
+  explicit Array(std::size_t size)
       : Container{size}, m_data{std::make_unique<T[]>(size)} {}
 
-  Vector(std::initializer_list<T> list)
-      : Vector(list.size()), m_data{std::make_unique<T[]>(m_size)} {
+  Array(std::initializer_list<T> list) : Array(list.size()) {
 
     std::copy(list.begin(), list.end(), m_data.get());
   }
+
+  using Index = std::ptrdiff_t;
+
+  const T &operator[](Index index) const { return m_data[index]; }
+
+  T &operator[](Index index) {
+    return const_cast<T &>(std::as_const(*this)[index]);
+  }
 };
+
+int main(int argc, char **argv) {
+
+  Array<int> arr(10);
+
+  arr[2] = 3;
+
+  auto n = arr.size();
+
+  for (auto i{0}; i < n; ++i) {
+    std::cout << arr[i] << '\n';
+  }
+
+  return 0;
+}

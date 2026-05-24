fae187deaef0ca71998c931b0a183b4ca4a595dd
John Swift <johnswift@live.com>
2026-05-24 19:38:27 -0400

    Basic inheritance

---
+++ inherit/main.cpp
@@ -0,0 +1,22 @@
+#include <cstddef>
+#include <iostream>
+#include <memory>
+
+// A container parent class
+// with a size and a size getter
+class Container {
+public:
+  std::size_t m_size;
+
+  std::size_t size() { return m_size; }
+
+  Container(std::size_t size) : m_size{size} {}
+};
+
+// A vector class which publically inherits container
+template <typename T> class Vector : public Container {
+public:
+  std::unique_ptr<T[]> m_data;
+  Vector(std::size_t size)
+      : Container{size}, m_data{std::make_unique<T[]>(size)} {}
+};

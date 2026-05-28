20bb87179275a0df77382891d2583178d390aa5e
John Swift <johnswift@live.com>
2026-05-28 19:07:46 -0400

    Construct a complex

--- virtual/main.cpp
+++ virtual/main.cpp
@@ -4,11 +4,20 @@
 // virtual base classes ensure that if a derived class inherits
 // a class using one (or multiple, they construct only one underlying object)
 
-class Number {};
+class Number {
+public:
+  Number() { std::cout << "Constructing Number" << '\n'; }
+};
 
-class Integer : public virtual Number {};
+class Integer : public virtual Number {
+public:
+  Integer() { std::cout << "Constructing Integer" << '\n'; }
+};
 
-class Imaginary : public virtual Number {};
+class Imaginary : public virtual Number {
+public:
+  Imaginary() { std::cout << "Constructing Imaginary" << '\n'; }
+};
 
 // Complex inherits both integer and imaginary,
 // which both inherit number.
@@ -16,4 +25,13 @@ class Imaginary : public virtual Number {};
 // however,
 // because imaginary and integer implement number as a virtual base class,
 // they will share only a single instance
-class Complex : public Integer, public Imaginary {};
+class Complex : public Integer, public Imaginary {
+public:
+  Complex() { std::cout << "Constructing Complex" << '\n'; }
+};
+
+int main() {
+  Complex comp;
+
+  return 0;
+}

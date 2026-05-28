5b6948dd129e8aa77cb3637efa358e8be85a0b1b
John Swift <johnswift@live.com>
2026-05-28 19:08:59 -0400

    Added a bad file, which does not use virtual. You can see Number being constructed twice when that is compiled

---
+++ virtual/bad.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// This is ripped straight from learncpp,
+// virtual base classes ensure that if a derived class inherits
+// a class using one (or multiple, they construct only one underlying object)
+
+class Number {
+public:
+  Number() { std::cout << "Constructing Number" << '\n'; }
+};
+
+class Integer : public Number {
+public:
+  Integer() { std::cout << "Constructing Integer" << '\n'; }
+};
+
+class Imaginary : public Number {
+public:
+  Imaginary() { std::cout << "Constructing Imaginary" << '\n'; }
+};
+
+// Complex inherits both integer and imaginary,
+// which both inherit number.
+// normally, this would result in two Numbers being constructed,
+// however,
+// because imaginary and integer implement number as a virtual base class,
+// they will share only a single instance
+class Complex : public Integer, public Imaginary {
+public:
+  Complex() { std::cout << "Constructing Complex" << '\n'; }
+};
+
+int main() {
+  Complex comp;
+
+  return 0;
+}

255bb227ea5225b13b77f82bbf1f688722c43caa
John Swift <johnswift@live.com>
2026-05-30 16:34:55 -0400

    Read bytes from the file, add to array

--- compression/main.cpp
+++ compression/main.cpp
@@ -1,5 +1,15 @@
+#include "tree.h"
+
 #include <fstream>
 #include <iostream>
+#include <memory>
+
+struct CompareTree {
+  bool operator()(const std::unique_ptr<Tree> &a,
+                  const std::unique_ptr<Tree> &b) {
+    return a->m_count > b->m_count;
+  }
+};
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -13,6 +23,17 @@ int main(int argc, char *argv[]) {
   if (!input.is_open()) {
     std::cerr << "Failed to open input file: " << file_path << '\n';
     return 1;
+  }
+
+  int counts[256]{};
+
+  char byte;
+  while (input.get(byte)) {
+    counts[static_cast<unsigned char>(byte)]++;
+  }
+
+  for (int i{0}; i < 256; ++i) {
+    std::cout << static_cast<unsigned char>(i) << ": " << counts[i] << '\n';
   }
 
   return 0;

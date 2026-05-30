24f1e26c0791fb053957d56036f246d852a968ab
John Swift <johnswift@live.com>
2026-05-30 18:39:33 -0400

    Enum error types

--- exceptions/main.cpp
+++ exceptions/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <ostream>
 
 // Exceptions dont have a specific type in C++.
 // you can throw and catch pretty much any type
 
 void func_three();
+
+// Enums for error types
+
+enum class MyError {
+  Something,
+  Else,
+  Foo,
+  Bar,
+};
+
+std::ostream &operator<<(std::ostream &out, const MyError &in) {
+  switch (in) {
+  case MyError::Bar:
+    out << "Bar";
+    break;
+  case MyError::Foo:
+    out << "Foo";
+    break;
+  case MyError::Something:
+    out << "Something";
+    break;
+  case MyError::Else:
+    out << "Else";
+  }
+
+  return out;
+}
+
+void throws_my_error();
 
 int main(int argc, char *argv[]) {
 
@@ -16,6 +46,12 @@ int main(int argc, char *argv[]) {
   }
 
   func_three();
+
+  try {
+    throws_my_error();
+  } catch (MyError e) {
+    std::cout << e << '\n';
+  }
 
   return 0;
 }
@@ -41,3 +77,5 @@ void func_three() {
     std::cout << e << '\n';
   }
 }
+
+void throws_my_error() { throw MyError::Foo; }
